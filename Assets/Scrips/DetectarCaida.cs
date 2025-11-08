03c259dd594c0ddbd967a7ec1599fc6f2d0de0fc
DulMaria <ri.ma.ni.1231980@gmail.com>
2025-11-08 09:24:02 -0400

    se agrego el script de detectar caida y reiniciar juego en todos los niveles y en el epilogo

---
+++ Assets/Scrips/DetectarCaida.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+ 
+public class DetectarCaida : MonoBehaviour
+{
+private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Jugador cayó al vacío");
+            // Por ejemplo:
+            other.transform.position = new Vector3(12.193f, 0.045f, -3.964f); // Punto de respawn
+        }
+    }
+}
